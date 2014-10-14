64e50e437a0bb53c81badf674cb7c0c28e55da8c
Raf Smits <raf_smits@hotmail.com>
2014-10-14 15:49:54 +0200

    add keywords to movies and populate

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,7 +3,14 @@ class Movie < ActiveRecord::Base
   scope :recent, ->{ where('finished_on > ?', 1.week.ago) }
   scope :search, ->(keyword){ where(title: keyword) if keyword.present? }
 
+  before_save :set_keywords
+
   def finished?
     finished_on.present?
   end
+
+  protected
+    def set_keywords
+      self.keywords = [title, description].map(&:downcase).join(' ')
+    end
 end
