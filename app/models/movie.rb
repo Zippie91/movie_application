685acc71a5e6f8003818adf91a0e3e2e7194b264
Raf Smits <raf_smits@hotmail.com>
2014-10-14 09:18:12 +0200

    reimplemented finished? method"

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,8 @@
 class Movie < ActiveRecord::Base
   scope :finished, ->{ where.not(finished_on: nil) }
   scope :recent, ->{ where('finished_on > ?', 1.week.ago) }
+
+  def finished?
+    finished_on.present?
+  end
 end
