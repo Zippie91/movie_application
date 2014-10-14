b1696408efa3d96dc123d3128cae6f51ab9c64a3
Raf Smits <raf_smits@hotmail.com>
2014-10-14 16:05:36 +0200

    Improved search

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,16 @@
 class Movie < ActiveRecord::Base
+  scope :finished, ->{ where.not(finished_on: nil) }
+  scope :recent, ->{ where('finished_on > ?', 1.week.ago) }
+  scope :search, ->(keyword){ where('keywords LIKE ?', "%#{keyword.downcase}%") if keyword.present? }
+
+  before_save :set_keywords
+
   def finished?
     finished_on.present?
   end
+
+  protected
+    def set_keywords
+      self.keywords = [title, description].map(&:downcase).join(' ')
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141014083731) do
+ActiveRecord::Schema.define(version: 20141014134111) do
 
   create_table "movies", force: true do |t|
     t.string   "title"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20141014083731) do
     t.date     "finished_on"
     t.integer  "rating"
     t.integer  "tmdbid"
+    t.text     "keywords"
   end
 
 end
