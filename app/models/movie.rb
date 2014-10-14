899540b2bc53252400b5b50d573bd84c38ad8d5f
Raf Smits <raf_smits@hotmail.com>
2014-10-14 10:35:04 +0200

    implement styles

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,7 +1,4 @@
 class Movie < ActiveRecord::Base
-  scope :finished, ->{ where.not(finished_on: nil) }
-  scope :recent, ->{ where('finished_on > ?', 1.week.ago) }
-
   def finished?
     finished_on.present?
   end
