f50fbcb6edd3f475a51967e3559a547256efdfd1
Raf Smits <raf_smits@hotmail.com>
2014-10-14 15:13:58 +0200

    better search

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,12 @@
 class Movie < ActiveRecord::Base
+  def self.search(keyword)
+    if keyword.present?
+      where(title: keyword)
+    else
+      all
+    end
+  end
+
   def finished?
     finished_on.present?
   end
